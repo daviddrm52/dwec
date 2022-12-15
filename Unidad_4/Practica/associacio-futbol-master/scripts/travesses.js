89aeea593e7544d3c78eb122559e97bd0cd3e8ec
daviddrm52 <davidgranada2015@gmail.com>
2022-12-15 19:32:24 +0100

    Lista de equipos añadida, trabajando en jornada

--- Unidad_4/Practica/associacio-futbol-master/scripts/travesses.js
+++ Unidad_4/Practica/associacio-futbol-master/scripts/travesses.js
@@ -1,6 +1,20 @@
 primeraMasc = ["Almería", "Athletic Club", "Atlético de Madrid", "FC Barcelona", "Real Betis", "Cádiz", "Celta de Vigo", "Elche", "Espanyol", "Getafe", "Girona", "Real Mallorca", "Osasuna", "Rayo Vallecano", "Real Madrid", "Real Sociedad", "Sevilla", "Valencia", "Valladolid", "Villarreal"];    
 segundaFem= ["Alavés", "Alhama", "Atlético Fem", "Barcelona Fem", "Tenerife", "Levante Fem", "Madrid CFF", "Betis Fem", "R. Madrid Fem", "R. Sociedad Fem", "Sevilla Fem", "Huelva Fem", "Valencia Fem", "Villarreal Fem"];
 
+var partMasc = document.getElementById("partidosMasculinos");
+var partFem = document.getElementById("partidosFemeninos");
+
+partMasc.innerHTML = "";
+partFem.innerHTML = "";
+  
+for (let i = 0; i < primeraMasc.length; i++) {
+    partMasc.innerHTML += "<li><span>"+primeraMasc[i]+"</span></li>";
+}
+
+for (let i = 0; i < segundaFem.length; i++) {
+    partFem.innerHTML += "<li><span>"+segundaFem[i]+"</span></li>";
+}
+
 const aleatorioMascLocal = primeraMasc[Math.floor(Math.random() * primeraMasc.length)];
 const aleatorioMascVisitante = primeraMasc[Math.floor(Math.random() * primeraMasc.length)];
 const aleatorioFemLocal = segundaFem[Math.floor(Math.random() * segundaFem.length)];
