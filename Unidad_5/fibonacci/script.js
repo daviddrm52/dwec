71d1d009f4bdaca33f115c4c37c517ec6a0f5c43
daviddrm52 <davidgranada2015@gmail.com>
2022-12-19 15:28:48 +0100

    Practica guiada de Fibonacci

---
+++ Unidad_5/fibonacci/script.js
@@ -0,0 +1,34 @@
+/* WEB WORKER */
+
+const botones = document.getElementsByTagName("button");
+const resultado = document.getElementById("resultado");
+
+let w;
+
+function startWebWorker() {
+    if(typeof(Worker) !== "undefined"){
+        if(typeof(w) == "undefined"){
+            w = new Worker("trabajador.js");
+        }
+        w.onmessage = function (event) {
+            resultado.innerHTML = event.data;
+        };
+    } else {
+        resultado.innerHTML = "Fallo al ejecutar el Worker!";
+    }
+};
+
+function stopWebWorker() {
+    w.terminate();
+    w = undefined;
+};
+
+//Eventos
+botones[0].addEventListener("click", ()=> {
+    console.log("b1");
+    startWebWorker();
+}, false);
+botones[1].addEventListener("click", ()=> {
+    console.log("b2");
+    stopWebWorker();
+}, false);
