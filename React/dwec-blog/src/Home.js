abd86b84868d4e270318acd418a688f448fc0749
daviddrm52 <davidgranada2015@gmail.com>
2023-01-23 17:20:48 +0100

    Avanzando

--- React/dwec-blog/src/Home.js
+++ React/dwec-blog/src/Home.js
@@ -1,14 +1,37 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import ListaBlogs from "./listaBlogs";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {titulo: 'Mi nueva web LowEffortGaming', body: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHH", autor: "Fimosis", id: 1 },
+    
+    
+    {/*const [blogs, setBlogs] = useState([
+        {titulo: 'Mi nueva web LowEffortGaming', body: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHH", autor: "Jesus", id: 1 },
         {titulo: 'Zona de recogida de basuras', body: "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE", autor: "Huberto", id: 2 },
         {titulo: 'Los moyai de las Islas de Pascua', body: "MOOOOOOOOOOOOOOAAAAAAAAAAAAAIIIIIIIII", autor: "Kesante", id: 3 },
         {titulo: 'Los juegos indie del momento', body: "AAAAAAAAAAAAHHHhhhhhhhhhhhhhhhh, kirby", autor: "Ana Spelunky", id: 4 },
         {titulo: 'World in Chaos, 5 años con el SCP:SL', body: "Te imaginas que se muere WiC, imposible", autor: "Jesus", id: 5 },
-    ]);
+    ]);*/}
+
+    const [blogs, setBlogs] = useState(null);
+
+    const [nombre, setNombre] = useState('Antonio');
+
+    const handleEliminarBlog = (id) => {
+        const blogsAux = blogs.filter(b => b.id != id);
+        setBlogs(blogsAux);
+    }
+
+    useEffect(() => {
+        console.log('useEffect disparado');
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            res.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+    }, []);
+
 
     //Jitterclick de mierda low effort
     const [click, setJitter] = useState(0);
@@ -18,7 +41,10 @@ const Home = () => {
 
     return ( 
         <div className="home">
-            <ListaBlogs blogs={blogs}/>
+            {/* <ListaBlogs blogs={ blogs } titulo="Listado completo de blogs" handleEliminarBlog={handleEliminarBlog}/>
+            <button onClick={() => setNombre('Huberto')}> Cambia nombre </button>
+            <p> {nombre} </p> */}
+            
             <div className="jitter" title="Low effort jitterclick">
                 <h3>Prueba del ratón</h3>
                 <p> Has realizado { click } clicks </p>
@@ -29,3 +55,17 @@ const Home = () => {
 }
  
 export default Home;
+
+{/*
+
+ENDPOINTS
+
+/blogs		GET		Devuelve todos los blogs
+/blogs/{id}		GET		Devuelve el blog con esa id
+/blogs/ 		POST		Añade un nuevo blog
+/blogs/{id} 	DELETE	Elimina un blog
+
+Comando para el JSON
+npx json-server --watch data/db.json --port 8000
+
+*/}
--- React/dwec-blog/src/listaBlogs.js
+++ React/dwec-blog/src/listaBlogs.js
@@ -1,13 +1,21 @@
-const ListaBlogs = ({blogs}) => {
+const ListaBlogs = (props) => {
+
+    const blogs = props.blogs;
+    const titulo = props.titulo;
+    const handleEliminarBlog = props.handleEliminarBlog;
+
+    
+
     return ( 
         <div className="content">
             <h1>Pagina de Inicio</h1>
-            <h2>¡Bienvenido!</h2>
+            <h2>{ titulo }</h2>
             { blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.titulo} </h2>
                     <p><b>Escrito por { blog.autor}</b></p>
                     <p><b>Descripción:</b> {blog.body}</p>
+                    <button onClick={() => handleEliminarBlog(blog.id)}>Eliminar blog</button>
                 </div>
             ))}
         </div>
